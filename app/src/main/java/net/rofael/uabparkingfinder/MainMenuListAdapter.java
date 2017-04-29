0b1567d805e8ab923d73041638933201a4cfbfcd
rkalz <aleezada@gmail.com>
2017-04-28 20:27:48 -0500

    Added check for newest report in MainMenuListAdapter. Changed LinearLayout to RelativeLayour for flexibility in item position

--- app/src/main/java/net/rofael/uabparkingfinder/MainMenuListAdapter.java
+++ app/src/main/java/net/rofael/uabparkingfinder/MainMenuListAdapter.java
@@ -2,14 +2,25 @@ package net.rofael.uabparkingfinder;
 
 import android.net.Uri;
 import java.util.ArrayList;
+import java.util.Collections;
+
 import android.app.Activity;
 import android.media.Image;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by aleez on 4/28/2017.
@@ -25,17 +36,46 @@ public class MainMenuListAdapter extends ArrayAdapter<Parking> {
         this.lotList = lotList;
     }
 
-    public View getView(int position, View view, ViewGroup parent)
+    public View getView(final int position, View view, ViewGroup parent)
     {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.main_menu_list,null,true);
 
         ImageView mapImg = (ImageView) rowView.findViewById(R.id.map);
         TextView name = (TextView) rowView.findViewById(R.id.parking_name);
+        final TextView status = (TextView) rowView.findViewById(R.id.last_report_time);
 
-        Uri placeholder = Uri.parse("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY");
+        Uri placeholder = Uri.parse("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=5&size=50x50");
         mapImg.setImageURI(placeholder);
         name.setText(lotList.get(position).toString());
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child(lotList.get(position).toString()).limitToLast(1).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot reportSnapshot : dataSnapshot.getChildren()) {
+                    // Adds a report from Firebase to the list if it hasn't been downloaded
+                    long reportTime = (long) reportSnapshot.child("reportTime").getValue();
+                    long reportStatus = (long) reportSnapshot.child("status").getValue();
+                    int reportStat = Integer.parseInt(Long.toString(reportStatus));
+                    Report rep = new Report(lotList.get(position), reportStat, reportTime);
+                    if (rep != null) {
+                        status.setText("Last report: " + rep.readableLastReportTime());
+                    }
+                    else
+                    {
+                        status.setText("No reports yet");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            }
+        });
+
         return rowView;
     }
 
